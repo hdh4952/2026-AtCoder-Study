a4a0e804717aa4749a63b05c7575792a6732bd62
nandarina7PNU <nandarina7@pusan.ac.kr>
2026-04-06 15:29:25 +0900

    B: AC

---
+++ ABC452/nandarina7/B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int h, w;
+    cin >> h >> w;
+    for(int i=0;i<h;++i){
+        for(int j=0;j<w;++j){
+            if(i==0 || j==0 || i==h-1 || j==w-1){
+                cout << "#";
+            }else{
+                cout << ".";
+            }
+        }
+        cout << '\n';
+    }
+    return 0;
+}
