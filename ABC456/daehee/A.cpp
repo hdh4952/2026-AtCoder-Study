1e5e14cd6363d4be0cc3416e74b0aacd3490dd49
Daehee Han <gkseogml4959@gmail.com>
2026-05-09 20:12:19 +0900

    A: AC

---
+++ ABC456/daehee/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int X; cin >> X;
+  if (3 <= X && X <= 18) cout << "Yes";
+  else cout << "No";
+
+}
